c7663e5c1cdf5bd119fb183018466a1e82bf5f26
SpeedRelativity <necharkc5150@gmail.com>
2026-06-01 12:45:31 -0700

    ingestion complete

--- README.md
+++ README.md
@@ -10,8 +10,14 @@ First I created a virtual environment and installed the necessary dependencies:
 
 ```bash
 python -m venv venv
-venv\Scripts\activate # because Im on windows.
-pip install langchain langchain-community langchain_text_splitters langchain-google-genai supabase # Im using genai cuz its free rather than openai which is paid.
+venv\Scripts\activate # because Im on windows. on linux its source venv/bin/activate
+pip install langchain langchain-community langchain_text_splitters langchain-google-genai supabase tqdm # Im using genai cuz its free rather than openai which is paid.
 ```
 
-Next, I wrote a function for reading the text files called
+Next, I wrote scrit for ingestion of the text files.
+
+But Im running into a rate limit problem. The free tier of gemini-embedding-001 only allows 100 requests/minute. I am trying to embed 5816 chunks at once.
+
+So solution for this is to create a batching system and wait in between. I will build this project FREE of cost, even though the whole thing would cost me a few cents if I paid.
+
+So the idea is I import time, create a for loop that creates batches and then runs the vector store function and has a sleep/wait timer.
--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -6,6 +6,9 @@ from langchain_community.document_loaders import TextLoader, DirectoryLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import SupabaseVectorStore
+from supabase import create_client
+
+import time
 
 
 docs_path = "docs"
@@ -21,13 +24,38 @@ def split__documents(documents, chunk_size=1000, chunk_overlap=150):
     text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     chunks = text_splitter.split_documents(documents)
     print(f"Split into {len(chunks)} chunks.")
-    return chunks[0:5]
+    return chunks
 
-def createEmbeddings(chunks):
-    embeddingModel = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001", dimensions=768, api_key=os.getenv("GEMINI_API_KEY"))
-    vectors = embeddingModel.embed_documents([chunk.page_content for chunk in chunks])
-    # next I need to store. 
-    vector_store = SupabaseVectorStore.from_vectors(vectors, chunks, table_name="documents")
+def createEmbeddings(chunks, batch_size=50):
+    
+    # First I'll create the client.
+    url = os.getenv("SUPABASE_URL")
+    key = os.getenv("SUPABASE_KEY")
+    supabase_client = create_client(url, key)
+
+    # Then I need the embedding model.
+    embedding_model = GoogleGenerativeAIEmbeddings(
+        model="gemini-embedding-001",
+        output_dimensionality=768,
+        google_api_key=os.getenv("GOOGLE_API_KEY"))
+    
+    # Finally, I can create the vector store, I need to batch though so a for loop with wait timer.
+
+    for i in range(0, len(chunks), batch_size):
+        batch = chunks[i:i+batch_size]
+        vector_store = SupabaseVectorStore.from_documents(
+        documents=batch,
+        embedding=embedding_model,
+        client=supabase_client,
+        table_name="documents",
+        query_name="match_documents"
+        )
+        print(f"Processed batch {i//batch_size + 1} of {len(chunks)//batch_size + 1}")
+        time.sleep(30) # to avoid rate limits
+        
+    
+    
+
     return vector_store
 
 
@@ -41,11 +69,11 @@ def main():
     # chunking the files
     print("Splitting documents into chunks...")
     chunks = split__documents(documents)
+    chunks = chunks[0:100] # for testing, rate limit.
 
     # embedding
     print("Creating embeddings and storing in vector database...")
     embeddings = createEmbeddings(chunks)
-    print(embeddings)
     print("Ingestion pipeline completed.") 
 
 
